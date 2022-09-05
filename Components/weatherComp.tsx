8d9be0ff8fb5f10213edc1a7cfdf927445b7c217
rohit.karbari <rohitkarbari26@gmail.com>
2022-09-05 11:54:05 +0530

    css added

--- Components/weatherComp.tsx
+++ Components/weatherComp.tsx
@@ -6,12 +6,14 @@ import { WeatherList } from './Container';
 
 const WeatherComp = ({route, navigation} : NativeStackScreenProps<WeatherList, 'WeatherComp'> ) => {
   return (
-    <View>
+    <View style={styles.wether}>
         <Text style={styles.heading}>Weather Condition</Text>
+        <View style= {styles.css}>
         <Image style={styles.logo} source={{uri: route.params?.icon}}/>
         <Text>Temperature :- {route.params?.temperature}</Text>
         <Text>Wind_speed :-{route.params?.wind_speed}</Text>
         <Text>Precipitation:-{route.params?.precipitation}</Text>
+        </View>
     </View>
   )
 }
@@ -27,6 +29,13 @@ const styles = StyleSheet.create({
        margin: 10,
        textAlign: 'center',
        fontWeight: 'bold'
+      },
+      wether: {
+        margin : 40,
+        backgroundColor: 'lightblue'
+      },
+      css: {
+        margin : 20
       }
 })
 export default WeatherComp
